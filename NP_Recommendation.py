00a0469cc7fb6b03845df5f0b185cbd250f63608
Charlie Wright <115735386+cwrig16@users.noreply.github.com>
2022-12-06 22:28:58 -0600

    Add files via upload
    Expanded logic to accept multiple selections at once by the user.

--- NP_Recommendation.py
+++ NP_Recommendation.py
@@ -1,71 +1,3 @@
-# nps_and_features = {
-# 'Acadia':set(['boating', 'camping', 'cross country skiing', 'fishing', 'forest', 'geology', 'hiking', 'mountains', 'water', 'wildlife']),
-# 'American Samoa':set(['diving', 'geology', 'hiking', 'forest', 'volcanos', 'water']),
-# 'Arches':set(['archeology', 'backcountry', 'camping', 'desert', 'geology', 'hiking', 'sand', 'wildlife']),
-# 'Badlands':set(['camping', 'geology', 'plains', 'wildlife']),
-# 'Big Bend':set(['archeology', 'backcountry', 'boating', 'camping', 'desert', 'fishing', 'forest', 'geology', 'hiking', 'mountains', 'water', 'wildlife']),
-# 'Biscayne':set(['backcountry', 'boating', 'camping', 'fishing', 'water', 'wildlife']),
-# 'Black Canyon of the Gunnison':set(['backcountry', 'boating', 'camping', 'cross country skiing', 'fishing', 'forest', 'geology', 'hiking', 'mountains', 'snowshoeing', 'water', 'wildlife']),
-# 'Bryce Canyon':set(['backcountry', 'camping', 'desert', 'forest', 'geology', 'hiking']),
-# 'Canyonlands':set(['backcountry', 'boating', 'camping', 'desert', 'fishing', 'geology', 'hiking', 'water', 'wildlife']),
-# 'Capitol Reef':set(['backcountry', 'camping', 'geology', 'fishing', 'hiking', 'water']),
-# 'Carlsbad Caverns':set(['backcountry', 'camping', 'caves', 'hiking']),
-# 'Channel Islands':set(['boating', 'camping', 'diving', 'water', 'wildlife']),
-# 'Congaree':set(['boating', 'fishing', 'forest', 'hiking', 'water', 'wildlife']),
-# 'Crater Lake':set(['backcountry', 'camping', 'fishing', 'forest', 'hiking', 'mountains', 'water', 'volcanoes']),
-# 'Cuyahoga Valley':set(['boating', 'cross country skiing', 'fishing', 'forest', 'geology', 'hiking', 'water', 'wildlife']),
-# 'Death Valley':set(['backcountry', 'camping', 'desert', 'geology', 'hiking', 'mountains']),
-# 'Denali':set(['backcountry', 'camping', 'hiking', 'mountains', 'wildlife']),
-# 'Dry Tortugas':set(['boating', 'camping', 'diving', 'fishing', 'water', 'wildlife']),
-# 'Everglades':set(['backcountry', 'boating', 'camping', 'fishing', 'hiking', 'water', 'wildlife']),
-# 'Gates of the Arctic':set(['backcountry', 'fishing', 'mountains', 'wildlife']),
-# 'Gettysberg':set(['camping', 'forest', 'hiking', 'military']),
-# 'Glacier':set(['archeology', 'boating', 'camping', 'cross country skiing', 'forest', 'geology', 'hiking', 'plains', 'mountains', 'snowshoeing', 'water', 'wildlife']),
-# 'Glacier Bay':set(['boating', 'fishing', 'forest', 'hiking', 'mountains', 'water', 'wildlife']),
-# 'Grand Canyon':set(['backcountry', 'boating', 'camping', 'desert', 'fishing', 'geology', 'hiking', 'water', 'wildlife']),
-# 'Grand Teton':set(['backcountry', 'boating', 'camping', 'cross country skiing', 'fishing', 'forest', 'hiking', 'mountains', 'snowshoeing', 'water', 'wildlife']),
-# 'Great Basin':set(['archeology', 'backcountry', 'camping', 'caves', 'desert', 'fishing', 'forest', 'geology', 'hiking', 'mountains', 'water', 'wildlife']),
-# 'Great Sand Dunes':set(['camping', 'desert', 'forest', 'hiking', 'sand', 'wildlife']),
-# 'Great Smokey Mountains':set(['backcountry', 'camping', 'fishing', 'forest', 'hiking', 'mountains']),
-# 'Guadalupe Mountains':set(['backcountry', 'camping', 'desert', 'geology', 'hiking', 'mountains', 'wildlife']),
-# 'Haleakala':set(['camping', 'desert', 'forest', 'geology', 'hiking', 'mountains', 'volcanoes']),
-# 'Hawaii Volcanoes':set(['backcountry', 'camping', 'geology', 'hiking', 'water', 'wildlife', 'volcanoes']),
-# 'Harpers Ferry':set(['forest', 'hiking', 'wildlife']),
-# 'Hot Springs':set(['archeology', 'camping', 'hiking', 'thermal features', 'water']),
-# 'Indiana Dunes':set(['hiking', 'sand', 'water', 'wildlife']),
-# 'Isle Royale':set(['archeology', 'backcountry', 'boating', 'camping', 'fishing', 'forest', 'geology', 'hiking', 'water', 'wildlife']),
-# 'Joshua Tree':set(['camping', 'desert', 'geology', 'hiking', 'mountains']),
-# 'Katmai':set(['backcountry', 'camping', 'fishing', 'hiking', 'mountains', 'wildlife']),
-# 'Kenai Fjords':set(['backcountry', 'boating', 'camping', 'hiking', 'mountains', 'water', 'wildlife']),
-# 'Kings Canyon':set(['forest', 'geology', 'hiking', 'mountains', 'water', 'wildlife']),
-# 'Kobuk Valley':set(['backcountry', 'fishing', 'forest', 'hiking', 'mountains', 'wildlife']),
-# 'Lake Clark':set(['fishing', 'forest', 'hiking', 'mountains', 'water', 'wildlife']),
-# 'Lassen Volcanic':set(['camping', 'cross country skiiing', 'fishing', 'hiking', 'snowshoeing', 'volcanoes']),
-# 'Mammoth Cave':set(['boating', 'camping', 'caves', 'fishing', 'forest', 'geology', 'hiking', 'wildlife']),
-# 'Mesa Verde':set(['archeology', 'geology', 'hiking', 'wildlife']),
-# 'Mount Rainier':set(['backcountry', 'camping', 'forest', 'hiking', 'mountains', 'water', 'wildlife']),
-# 'North Cascades':set(['backcountry', 'camping', 'fishing', 'forest', 'geology', 'hiking', 'mountains', 'water', 'wildlife']),
-# 'Organ Pipe Cactus':set(['desert', 'hiking', 'wildlife']),
-# 'Olympic':set(['backcountry', 'boating', 'camping', 'fishing', 'forest', 'hiking', 'mountains', 'sand', 'water', 'wildlife']),
-# 'Petrified Forest':set(['backcountry', 'desert', 'geology', 'hiking']),
-# 'Pinnacles':set(['caves', 'hiking', 'mountains', 'water', 'wildlife', 'volcanoes']),
-# 'Redwood':set(['backcountry', 'fishing', 'forest', 'hiking', 'water', 'wildlife']),
-# 'Rocky Mountain':set(['backcountry', 'camping', 'fishing', 'forest', 'hiking', 'mountains', 'wildlife']),
-# 'Saguaro':set(['desert', 'forest', 'hiking', 'wildlife']),
-# 'Sequoia':set(['caves', 'forest', 'geology', 'hiking', 'mountains', 'water', 'wildlife']),
-# 'Shenandoah':set(['backcountry', 'camping', 'fishing', 'forest', 'hiking', 'mountains', 'water', 'wildlife']),
-# 'Theodore Roosevelt':set(['camping', 'cross country skiing', 'hiking', 'plains', 'wildlife']),
-# 'Valley Forge':set(['hiking', 'military', 'wildlife']),
-# 'Virgin Islands':set(['archeology', 'boating', 'desert', 'diving', 'fishing', 'forest', 'hiking', 'sand', 'water', 'wildlife']),
-# 'Voyageurs':set(['boating', 'camping', 'fishing', 'water']),
-# 'Wind Cave':set(['backcountry', 'camping', 'caves', 'forest', 'hiking', 'plains', 'wildlife']),
-# 'White Sands':set(['backcountry', 'camping', 'desert', 'geology', 'hiking', 'sand', 'wildlife']),
-# 'Wrangell-St. Elias':set(['backcountry', 'forest', 'geology', 'hiking', 'mountains', 'water', 'wildlife']),
-# 'Yellowstone':set(['archeology', 'backcountry', 'boating', 'camping', 'cross country skiing', 'fishing', 'forest', 'geology', 'hiking', 'military', 'mountains', 'snowshoeing', 'thermal features', 'water', 'wildlife', 'volcanoes']),
-# 'Yosemite':set(['camping', 'fishing', 'forest', 'geology', 'hiking', 'mountains', 'water', 'wildlife']),
-# 'Zion':set(['backcountry', 'camping', 'desert', 'fishing', 'forest', 'geology', 'hiking', 'mountains', 'water', 'wildlife'])
-# }
-
 nps_and_features = {
 'Acadia':'cdfijklnst',
 'American Samoa':'hkljsu',
@@ -166,7 +98,7 @@ def show_features():
     print(feature_string)
 
 def greet():
-    print('Welcome to National Park Recommendation!!! \nBy selecting a feature of the National Parks, you will be given a list of recommended parks to visit.\n')
+    print('\nWelcome to National Park Recommendation!!! \nBy selecting a feature of the National Parks, you will be given a list of recommended parks to visit.\n')
 
 def farewell():
     print('Thank you for using National Park Recommendation.')
@@ -193,11 +125,16 @@ def new_selection():
 
 def get_feature():
     while True:
-        feature = input('Please select a feature by typing the cooresponding letter.\nYour selection: ')
-        if feature.lower() in features.keys():
-            chosen_feature = features[feature]
-            print(f'\nYou selected {feature} - {chosen_feature}.\n')
-            return feature
+        feature = input('Please select one or more features by typing the cooresponding letter(s) below without spaces or commas.\nYour selection(s): ')
+        chosen_features = ''
+        choices = []
+        for letter in feature:
+            if letter.lower() in features.keys() and letter.lower() not in choices:
+                chosen_features += f'{features[letter]}\n' 
+                choices.append(letter)     
+        if len(feature) == len(choices):
+            print(f'\nYou selected:\n{chosen_features}\n')
+            return choices
         else:
             print('\nInvalid selection.  Please try again.\n')
             continue
@@ -206,10 +143,13 @@ def give_recommendation():
     selection = get_feature()
     recommendation = '\n'
     for key, value in nps_and_features.items():   
-        if selection in value:
+        if all([char in value for char in selection]):
             recommendation += f'{key}\n'
-    print(f'Your recommneded National Parks to visit are: \n{recommendation}')
-    
+    if len(recommendation) > 2:
+        print(f'Your recommneded National Parks to visit are: \n{recommendation}')
+    else:
+        print(f'Unable to recommend National Park with all of your selections.\n')
+
 def np_recommendation():
     greet()
     show_features()
